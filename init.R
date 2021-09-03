f9ebfc53889413298c435175d5be0bd24abf467a
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2021-09-03 12:52:12 -0600

    install packages in init.R

--- init.R
+++ init.R
@@ -10,7 +10,20 @@ options(repos=r)
 # ============================================================== #
 
 # packages go here
-install.packages('remote')
+install.packages('remotes', dependencies = TRUE)
+install.packages("quantmod", dependencies = TRUE)
+install.packages("zoo", dependencies = TRUE)
+install.packages("tidyr", dependencies = TRUE)
+install.packages("dplyr", dependencies = TRUE)
+install.packages("xts", dependencies = TRUE)
+install.packages("moments", dependencies = TRUE)
+install.packages("MASS", dependencies = TRUE)
+install.packages("QRM", dependencies = TRUE)
+install.packages("plotly", dependencies = TRUE)
+install.packages("dash", dependencies = TRUE)
+install.packages("dashCoreComponents", dependencies = TRUE)
+install.packages("dashHtmlComponents", dependencies = TRUE)
+install.packages("dashBootstrapComponents", dependencies = TRUE)
 
 remotes::install_github(
 			'plotly/dashR',
